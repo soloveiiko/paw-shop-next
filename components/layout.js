fb6a6232730854e6e723fc0a9f73e99621675ce8
soloveiiko <soloveymaria51@gmail.com>
2023-10-20 17:49:36 +0300

    added: local font

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Header from '@components/Base/Header/Header';
 import Footer from '@components/Base/Footer/Footer';
-import { Montserrat_Alternates, Montserrat, Amatic_SC } from 'next/font/google';
-
-const montserrat_alternates = Montserrat_Alternates({
+import { Montserrat_Alternates, Montserrat } from 'next/font/google';
+import localFont from 'next/font/local';
+export const montserrat_alternates = Montserrat_Alternates({
   weight: ['300', '400', '500', '700', '900'],
   style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
+  variable: '--font-montserrat_alternates',
 });
-export const amatic_sc = Amatic_SC({
-  weight: ['700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
+// export const amatic_sc = Amatic_SC({
+//   weight: ['700'],
+//   style: ['normal'],
+//   subsets: ['latin'],
+//   display: 'swap',
+// });
+export const amatic_sc = localFont({
+  src: [
+    {
+      path: './../src/assets/fonts/Amatic_SC/AmaticSC-Bold.woff2',
+      weight: '700',
+    },
+  ],
 });
+
 export const montserrat = Montserrat({
   weight: ['500'],
   style: ['normal'],
@@ -23,7 +33,15 @@ export const montserrat = Montserrat({
 });
 function Layout({ children }) {
   return (
-    <div className={`paw-shop ${montserrat_alternates.className}`}>
+    <div className={`paw-shop`}>
+      <style jsx global>{`
+        :root {
+          --font-montserrat_alternates: ${montserrat_alternates.style
+            .fontFamily};
+          --font-amatic_sc: ${amatic_sc.style.fontFamily};
+          --font-montserrat: ${montserrat.style.fontFamily};
+        }
+      `}</style>
       <Header />
       <main>{children}</main>
       <Footer />
